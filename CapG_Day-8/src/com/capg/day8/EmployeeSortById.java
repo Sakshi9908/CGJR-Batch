c1147d33bffda9365378e6e2c0f59ad39fa31492
Sakshi9908 <sakshishah773@gmail.com>
2022-02-07 15:46:08 +0530

    Day 8 added

---
+++ CapG_Day-8/src/com/capg/day8/EmployeeSortById.java
@@ -0,0 +1,21 @@
+package com.capg.day8;
+import java.util.Comparator;
+public class EmployeeSortById implements Comparator<Employee>{
+
+	@Override
+	public int compare(Employee e1, Employee e2) {
+		// TODO Auto-generated method stub
+		if(e1.getId()>e2.getId())
+		{
+			return 1;
+		}
+		else if(e1.getId()<e2.getId())
+		{
+			return -1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+}
